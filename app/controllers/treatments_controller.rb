b2a1020281c0146d09446957cc05f0f404f8edc7
deborah <deborah@70dedb63-593e-0410-87e1-edbeff3ae1a3>
2007-09-21 13:47:18 +0000

    git-svn-id: https://mydrugref.googlecode.com/svn/trunk@16 70dedb63-593e-0410-87e1-edbeff3ae1a3

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -4,17 +4,6 @@ class TreatmentsController < PostsController
     super
     @post_pages, @posts = paginate :treatments, :order_by => 'created_at desc', :per_page => 5
   end
-
-# add_tag is not currently being used
-  def add_tag
-    @treat = Treatment.find(params[:id])
-    # changed to reflect latest version of acts_as_taggable_on_steroids
-    @treat.tag_list.add("Psychiatry and Behaviour")
-    if @treat.save
-      flash[:notice] = 'Tag has been added.'
-    end
-    redirect_to :action => 'show', :id => @treat
-  end
   
   def remove_tag
     @post = Treatment.find(params[:id])

f23614fd8deba40176e3beb4b2543f2b7bcecbec
deborah <deborah@70dedb63-593e-0410-87e1-edbeff3ae1a3>
2007-10-05 19:14:47 +0000

    added company
    git-svn-id: https://mydrugref.googlecode.com/svn/trunk@31 70dedb63-593e-0410-87e1-edbeff3ae1a3

---
+++ app/models/company.rb
@@ -0,0 +1,19 @@
+class Company < ActiveRecord::Base
+set_table_name "cd_companies"
+acts_as_ferret
+belongs_to :drug, :foreign_key => "drug_code"
+
+def self.full_text_search(q, options = {})
+   return nil if q.nil? or q==""
+   default_options = {:limit => 10, :page => 1}
+   options = default_options.merge options
+ 
+   # get the offset based on what page we're on
+   options[:offset] = options[:limit] * (options.delete(:page).to_i-1)
+ 
+   # now do the query with our options
+   results = Company.find_by_contents(q, options)
+   return [results.total_hits, results]
+  end
+  
+end

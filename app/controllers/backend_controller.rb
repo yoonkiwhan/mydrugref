197818266c7909811cbc48b8987ec974c9d1154a
deborahlchan <deborahlchan@70dedb63-593e-0410-87e1-edbeff3ae1a3>
2008-03-20 20:15:16 +0000

    git-svn-id: https://mydrugref.googlecode.com/svn/trunk@54 70dedb63-593e-0410-87e1-edbeff3ae1a3

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -9,24 +9,33 @@ class BackendController < ApplicationController
      Post.find_all_by_atc(atc)
   end 
      
-  def fetch(method, atcs, email = "")
+  def fetch(method, atcs, email = "", inclusive = true)
     
   default_email = "none"
   if email == nil
   email = default_email
   end
     
-  if email != "none"
+     if email != "none"
   
-  @user = User.find_by_email("#{email}")
-  @friend_ids = Array.new
-  @friend_ids << @user.id
-    
-    for pal in @user.friends
-    @friend_ids << pal.id
-    end
-    
+     @user = User.find_by_email("#{email}")
   
+        if @user == nil
+        email = "none"
+
+        else
+
+           @friend_ids = Array.new
+           @friend_ids << @user.id
+    
+           for pal in @user.friends
+              @friend_ids << pal.id
+           end
+    
+         end     
+
+      end 
+
     if method == "interactions_byATC"
       @results = Array.new
         for atc in atcs
@@ -37,13 +46,29 @@ class BackendController < ApplicationController
           @results = @results + @array
           end
         end
+
+       if email == "none"
+          return @results
+       
+       else
+
       @finalresults = Array.new
+
       for post in @results
         if @friend_ids.include?(post.created_by)
-          @finalresults << post
+           if inclusive == true
+             post.attributes["trusted"] = true
+           elsif inclusive == false
+             @finalresults << post
+           end
+        end
+        if inclusive == true
+           @finalresults << post
         end
       end
+
       @results_ids = Array.new
+
       for result in @results
         @results_ids << result.id
       end
@@ -51,115 +76,131 @@ class BackendController < ApplicationController
       @comments = Comment.find_all_by_goat(true)
       for c in @comments
         if @friend_ids.include?(c.created_by) and @results_ids.include?(c.post_id)
-          @finalresults << Post.find(c.post_id)
-        end
-      end 
-      return @finalresults
-    end
-    
-    if method == "warnings_byATC"
-      @results = Array.new
-      for atc in atcs
-        @array = Warning.find_all_by_atc(atc)
-        @results = @results + @array
-      end
-      @finalresults = Array.new
-      for post in @results
-        if @friend_ids.include?(post.created_by)
-          @finalresults << post
-        end
-      end
-      @results_ids = Array.new
-      for result in @results
-        @results_ids << result.id
-      end
-      
-      @comments = Comment.find_all_by_goat(true)
-      for c in @comments
-        if @friend_ids.include?(c.created_by) and @results_ids.include?(c.post_id)
-          @finalresults << Post.find(c.post_id)
-        end
-      end 
-      return @finalresults
-    end
-    
-    if method == "bulletins_byATC"
-      @results = Array.new
-      for atc in atcs
-        @array = Bulletin.find_all_by_atc(atc)
-        @results = @results + @array
-      end
-      @finalresults = Array.new
-      for post in @results
-        if @friend_ids.include?(post.created_by)
-          @finalresults << post
-        end
-      end
-      @results_ids = Array.new
-      for result in @results
-        @results_ids << result.id
-      end
-      
-      @comments = Comment.find_all_by_goat(true)
-      for c in @comments
-        if @friend_ids.include?(c.created_by) and @results_ids.include?(c.post_id)
-          @finalresults << Post.find(c.post_id)
-        end
-      end 
-      return @finalresults
-    end     
-    
-    if method == "prices_byATC"
-      @results = Array.new
-      for atc in atcs
-        @products = Product.find_all_by_atc(atc)
-        for product in @products
-          @prices = product.prices
-          @results = @results + @prices
-        end
-      end
-      @finalresults = Array.new
-      for post in @results
-        if @friend_ids.include?(post.created_by)
-          @finalresults << post
-        end
-      end
-      return @finalresults
-    end
-  
-  elsif email == "none"
-    
-    if method == "interactions_byATC"
-      @results = Array.new
-        for atc in atcs
-        @copy = Array.new(atcs)
-        @copy.delete(atc)
-          for code in @copy
-          @array = Interaction.find(:all, :conditions => ["atc = ? and atc2 = ?", atc, code])
-          @results = @results + @array
+          @post = Post.find(c.post_id)
+          if inclusive == true
+             @post.attributes["trusted"] = true
+          elsif inclusive == false   
+             @finalresults << @post
           end
         end
-      return @results
+        if inclusive == true
+           @finalresults << post
+        end
+      end 
+      return @finalresults
+    
+      end
+
     end
     
+
     if method == "warnings_byATC"
       @results = Array.new
       for atc in atcs
         @array = Warning.find_all_by_atc(atc)
         @results = @results + @array
       end
-      return @results
-    end
+ 
+       if email == "none"
+          return @results
+ 
+       else
+
+      @finalresults = Array.new
+      for post in @results
+        if @friend_ids.include?(post.created_by)
+           if inclusive == true
+             post.attributes["trusted"] = true
+           elsif inclusive == false
+             @finalresults << post
+           end
+        end
+        if inclusive == true
+           @finalresults << post
+        end
+      end
+
+      @results_ids = Array.new
+
+      for result in @results
+        @results_ids << result.id
+      end
+      
+      @comments = Comment.find_all_by_goat(true)
+      for c in @comments
+        if @friend_ids.include?(c.created_by) and @results_ids.include?(c.post_id)
+          @post = Post.find(c.post_id)
+          if inclusive == true
+             @post.attributes["trusted"] = true
+          elsif inclusive == false   
+             @finalresults << @post
+          end
+        end
+        if inclusive == true
+           @finalresults << post
+        end
+      end 
+      return @finalresults
     
+      end
+
+    end
+
     if method == "bulletins_byATC"
       @results = Array.new
       for atc in atcs
         @array = Bulletin.find_all_by_atc(atc)
         @results = @results + @array
       end
-      return @results
-    end     
+
+      if email == "none"
+         return @results
+
+      else
+
+      @finalresults = Array.new
+      for post in @results
+        if @friend_ids.include?(post.created_by)
+           if inclusive == true
+             post.attributes["trusted"] = true
+           elsif inclusive == false
+             @finalresults << post
+           end
+        end
+        if inclusive == true
+           @finalresults << post
+        end
+      end
+
+      @results_ids = Array.new
+
+      for result in @results
+        @results_ids << result.id
+      end
+      
+      @comments = Comment.find_all_by_goat(true)
+      for c in @comments
+        if @friend_ids.include?(c.created_by) and @results_ids.include?(c.post_id)
+          @post = Post.find(c.post_id)
+          if inclusive == true
+             @post.attributes["trusted"] = true
+          elsif inclusive == false   
+             @finalresults << @post
+          end
+        end
+        if inclusive == true
+           @finalresults << post
+        end
+      end 
+      return @finalresults
     
+      end
+
+    end
+
+    end     
+
+
     if method == "prices_byATC"
       @results = Array.new
       for atc in atcs
@@ -169,21 +210,34 @@ class BackendController < ApplicationController
           @results = @results + @prices
         end
       end
-      return @results
-    end
-    end
-  end
-  
-  def narrow
-    @finalresults = Array.new
+
+      if email == "none"
+         return @results
+
+      else
+
+      @finalresults = Array.new
       for post in @results
         if @friend_ids.include?(post.created_by)
-        @finalresults << post
+	   if inclusive == true
+              post.attributes["trusted"] = true
+           elsif inclusive == false
+             @finalresults << post
+           end
         end
-      end 
-    return @finalresults
+        if inclusive == true
+           @finalresults << post
+        end
+      end
+      return @finalresults
+
+      end
+
+    end
+ 
   end
-    
+  
+
   def find_users_by_name(name)
     @page_title = "did it work?"
      User.find_all_by_name(name)
